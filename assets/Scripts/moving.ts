9835772aed66aa0299893d4d1b2b81623f725e38
Your Name <quenemdi831@gmail.com>
2023-02-22 09:08:01 +0700

    init project

---
+++ assets/Scripts/moving.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('moving')
+export class moving extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
